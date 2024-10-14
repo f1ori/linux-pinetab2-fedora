e49dd8a6606345e676e94695373a7db43b1f72b4
Thorsten Leemhuis <fedora@leemhuis.info>
2024-10-14 11:10:39 +0200

    Revert "tools/rtla: fix collision with glibc sched_attr/sched_set_attr"
    This reverts commit 4719d3c1b89bf1862675e0353db634b941af525b.

--- tools/tracing/rtla/src/utils.h
+++ tools/tracing/rtla/src/utils.h
@@ -46,7 +46,6 @@ update_sum(unsigned long long *a, unsigned long long *b)
 	*a += *b;
 }
 
-#ifndef SCHED_ATTR_SIZE_VER0
 struct sched_attr {
 	uint32_t size;
 	uint32_t sched_policy;
@@ -57,7 +56,6 @@ struct sched_attr {
 	uint64_t sched_deadline;
 	uint64_t sched_period;
 };
-#endif /* SCHED_ATTR_SIZE_VER0 */
 
 int parse_prio(char *arg, struct sched_attr *sched_param);
 int parse_cpu_set(char *cpu_list, cpu_set_t *set);
